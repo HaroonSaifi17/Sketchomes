34ee2186c32e85676ab68a49f65df99f2410e487
Mohd Haroon <haroonsaifi2001@gmail.com>
2024-04-11 22:23:50 +0530

    NavBar complete

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  @ViewChild('b1', { static: true }) b1!: ElementRef;
+  public navState: boolean = true;
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+    const scrollPosition = window.pageYOffset;
+    if (scrollPosition < 2) {
+      this.b1.nativeElement.style.height = '0';
+    } else {
+      this.b1.nativeElement.style.height = '100%';
+    }
+  }
 }
