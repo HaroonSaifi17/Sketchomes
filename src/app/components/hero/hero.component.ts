6a998f854de25944a595aeb368c69e9cc92a6d31
Mohd Haroon <haroonsaifi2001@gmail.com>
2024-04-16 17:58:54 +0530

    Hero Complete

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit{
+  public selectImg: number = 0;
 
+  ngOnInit() {
+    setInterval(() => {
+      if (this.selectImg == 1) {
+        this.selectImg = 0;
+      }
+      else{
+      this.selectImg = this.selectImg + 1;
+      }
+    },5000);
+  }
 }
