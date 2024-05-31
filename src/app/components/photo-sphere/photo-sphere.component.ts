275ebd591933e3df50c0e7658691858ca5b50cce
Haroon Saifi <haroonsaifi2001@gmail.com>
2024-05-31 23:09:22 +0530

    fix photosphere brightness

--- src/app/components/photo-sphere/photo-sphere.component.ts
+++ src/app/components/photo-sphere/photo-sphere.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'app-photo-sphere',
@@ -9,116 +10,73 @@ import * as THREE from 'three';
   styleUrls: ['./photo-sphere.component.css'],
 })
 export class PhotoSphereComponent implements AfterViewInit {
-  @ViewChild('container') container!: ElementRef;
+  @ViewChild('canvas', { static: true }) canvas!: ElementRef<HTMLCanvasElement>;
 
-  camera: THREE.PerspectiveCamera;
-  scene: THREE.Scene;
-  renderer: THREE.WebGLRenderer;
+  private camera!: THREE.PerspectiveCamera;
+  private scene!: THREE.Scene;
+  private renderer!: THREE.WebGLRenderer;
+  private sphere!: THREE.Mesh;
 
-  isUserInteracting = false;
-  onPointerDownMouseX = 0;
-  onPointerDownMouseY = 0;
-  lon = 0;
-  onPointerDownLon = 0;
-  lat = 0;
-  onPointerDownLat = 0;
-  phi = 0;
-  theta = 0;
-
-  constructor() {
-    this.camera = new THREE.PerspectiveCamera(70, 3 / 2, 1, 1100);
-    this.scene = new THREE.Scene();
-    this.renderer = new THREE.WebGLRenderer();
-  }
+  constructor() {}
 
   ngAfterViewInit(): void {
     this.init();
     this.animate();
   }
 
-  init(): void {
-    this.renderer.setSize(Math.min(1536, window.innerWidth), Math.min(1400/1.5,window.innerWidth / 1.5));
-    this.container.nativeElement.appendChild(this.renderer.domElement);
+  private init(): void {
+    const width = window.innerWidth;
+    const height = (window.innerWidth / 3) * 2;
 
-    this.container.nativeElement.style.touchAction = 'none';
-    this.container.nativeElement.addEventListener(
-      'pointerdown',
-      this.onPointerDown.bind(this),
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas.nativeElement,
+      antialias: true,
+    });
+    this.renderer.setSize(width, height, true);
+
+    this.scene = new THREE.Scene();
+
+    this.camera = new THREE.PerspectiveCamera(75, 3 / 2, 0.1, 1000);
+    this.camera.position.z = 0.01;
+
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.enableRotate = true;
+    controls.enablePan = false;
+
+    const texture = new THREE.TextureLoader().load(
+      '/assets/images/render.jpg',
+      (texture) => {
+        texture.colorSpace = THREE.SRGBColorSpace;
+        texture.minFilter = THREE.LinearFilter;
+        texture.magFilter = THREE.LinearFilter;
+        texture.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
+        const geometry = new THREE.SphereGeometry(100, 100, 40);
+        const material = new THREE.MeshBasicMaterial({
+          side: THREE.BackSide,
+          map: texture,
+        });
+        this.sphere = new THREE.Mesh(geometry, material);
+        this.scene.add(this.sphere);
+      },
     );
 
     window.addEventListener('resize', this.onWindowResize.bind(this));
-    const texture = new THREE.TextureLoader().load('/assets/images/render.jpg');
-    this.renderer.toneMapping = THREE.LinearToneMapping;
-    this.renderer.toneMappingExposure = 1;
-
-    const material = new THREE.MeshBasicMaterial({ map: texture });
-
-    const geometry = new THREE.SphereGeometry(500, 60, 40);
-    geometry.scale(-1, 1, 1);
-
-    const mesh = new THREE.Mesh(geometry, material);
-    this.scene.add(mesh);
   }
 
-  onWindowResize() {
+  private onWindowResize(): void {
+    const width = window.innerWidth;
+    const height = (window.innerWidth / 3) * 2;
     this.camera.aspect = 3 / 2;
     this.camera.updateProjectionMatrix();
-
-    this.renderer.setSize(Math.min(1536, window.innerWidth), Math.min(1400/1.5,window.innerWidth / 1.5));
+    this.renderer.setSize(width, height, true);
   }
 
-  onPointerDown(event: PointerEvent) {
-    if (event.button !== 0) return; // Check for left mouse button
-
-    this.isUserInteracting = true;
-
-    this.onPointerDownMouseX = event.clientX;
-    this.onPointerDownMouseY = event.clientY;
-
-    this.onPointerDownLon = this.lon;
-    this.onPointerDownLat = this.lat;
-
-    document.addEventListener('pointermove', this.onPointerMove.bind(this));
-    document.addEventListener('pointerup', this.onPointerUp.bind(this));
-  }
-
-  onPointerMove(event: PointerEvent) {
-    if (!this.isUserInteracting) return;
-
-    this.lon =
-      (this.onPointerDownMouseX - event.clientX) * 0.1 + this.onPointerDownLon;
-    this.lat =
-      (event.clientY - this.onPointerDownMouseY) * 0.1 + this.onPointerDownLat;
-  }
-
-  onPointerUp(event: PointerEvent) {
-    if (!this.isUserInteracting) return;
-
-    this.isUserInteracting = false;
-
-    document.removeEventListener('pointermove', this.onPointerMove.bind(this));
-    document.removeEventListener('pointerup', this.onPointerUp.bind(this));
-  }
-
-  animate() {
+  private animate(): void {
     requestAnimationFrame(() => this.animate());
-    this.update();
-  }
 
-  update() {
-    if (this.isUserInteracting === false) {
-      this.lon += 0.07;
+    if (this.sphere) {
+      this.sphere.rotation.y += 0.002;
     }
-
-    this.lat = Math.max(-85, Math.min(85, this.lat));
-    this.phi = THREE.MathUtils.degToRad(90 - this.lat);
-    this.theta = THREE.MathUtils.degToRad(this.lon);
-
-    const x = 500 * Math.sin(this.phi) * Math.cos(this.theta);
-    const y = 500 * Math.cos(this.phi);
-    const z = 500 * Math.sin(this.phi) * Math.sin(this.theta);
-
-    this.camera.lookAt(x, y, z);
 
     this.renderer.render(this.scene, this.camera);
   }
